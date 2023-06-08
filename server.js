2a9d6650073c8bc868460bdd49d85b4539c42588
mazenfteha <mazn34903@gmail.com>
2023-06-07 18:53:02 -0700

    create todo app

--- server.js
+++ server.js
@@ -7,7 +7,52 @@ const client = new Client({
     database: "todo"
 })
 
-client.connect()
-.then(() => console.log("Connected successfuly"))
-.catch(e => console.log)
-.finally(() => client.end())
+start()
+async function start() {
+    await connect();
+    const todos = await readTodos();
+    console.log(todos)
+
+    const successCreate = await createTodo();
+    console.log(`Creating was ${successCreate}`)
+
+    const successDelete = await deleteTodo(1)
+    console.log(`Deleting was ${successDelete}`)
+}
+
+async function connect() {
+    try {
+        await client.connect()
+        console.log('connect successfully');
+    } catch (e) {
+        console.error(`Failed to connect ${e}`)
+    }
+}
+
+async function readTodos() {
+    try {
+        const result = await client.query("select id, text from todos");
+        return result.rows;
+    } catch (error) {
+        return [];
+    }
+}
+
+async function createTodo() {
+    try {
+        await client.query("insert into todos values ($1, $2)",[1, 'read book']);
+        return true;
+    } catch (e) {
+        return false;
+    }
+
+}
+
+async function deleteTodo(id) {
+    try {
+        await client.query("delete from todos where id = $1", [id]);
+        return true
+    } catch (e) {
+        return false
+    }
+}
